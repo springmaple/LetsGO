d05177cdd7f0df10ed7b4d88495ebb243868a22c
springmaple <springmaple@live.com.my>
2019-10-05 17:28:31 +0800

    Remove old files

--- src/index.py
+++ src/index.py
@@ -1,81 +1,57 @@
-from collections import namedtuple
-
 import firebase_admin
-from firebase_admin import credentials
-from firebase_admin import firestore
+from firebase_admin import credentials, firestore
 
-from sql_script import SqlScript
-
-SqlData = namedtuple('SqlData', ('items', 'companies', 'company_branches'))
-
-
-def _get_sql_data() -> SqlData:
-    sql_data = SqlData([], [], [])
-    for i in SqlScript('get_stock_items'):
-        line = i.decode()
-        if line.startswith('i='):
-            code, desc, balance, uom, ref_cost, ref_price, _ = line[2:].split('|')
-            sql_data.items.append({
-                'code': code,
-                'desc': desc,
-                'balance': balance,
-                'uom': uom,
-                'ref_cost': ref_cost,
-                'ref_price': ref_price
-            })
-        elif line.startswith('c='):
-            company_code, company_name, _ = line[2:].split('|')
-            sql_data.companies.append({
-                'code': company_code,
-                'name': company_name
-            })
-        elif line.startswith('b='):
-            (company_code, branch_name, attention,
-             addr1, addr2, addr3, addr4,
-             phone1, phone2, fax1, fax2, email, _) = line[2:].split('|')
-            sql_data.company_branches.append({
-                'company_code': company_code,
-                'name': branch_name,
-                'attention': attention,
-                'address': [addr1, addr2, addr3, addr4],
-                'phone': [phone1, phone2],
-                'fax': [fax1, fax2],
-                'email': email
-            })
-
-    return sql_data
+from sql import Sql
 
 
 def _escape_key(key: str):
     return key.replace('/', '_')
 
 
-def start():
+def upload_master_data():
     cred = credentials.Certificate('../res/serviceAccount.json')
     firebase_admin.initialize_app(cred)
     db = firestore.client()
 
-    sql_data = _get_sql_data()
+    # Stock items
+    stocks = {}
+    for stock in sql.get_stock_items():
+        stocks[stock.code] = stock
 
-    for item in sql_data.items:
-        print('Uploading item ' + item['code'])
-        item_code = _escape_key(item['code'])
-        doc_ref = db.document('items/' + item_code)
-        doc_ref.set(item)
+    for uom in sql.get_stock_item_uom():
+        stocks[uom.code].uom.append(uom)
 
-    for company in sql_data.companies:
-        print('Uploading company ' + company['code'])
-        company_code = _escape_key(company['code'])
-        doc_ref = db.document('companies/' + company_code)
-        doc_ref.set(company)
+    for stock_code, stock in stocks.items():
+        print(f'Uploading stock {_escape_key(stock_code)}')
+        doc_ref = db.document(f'items/{_escape_key(stock_code)}')
+        doc_ref.set(stock.to_dict())
 
-    for branch in sql_data.company_branches:
-        print('Uploading company branch ' + branch['name'])
-        company_code = _escape_key(branch['company_code'])
-        branch_key = _escape_key(branch['name'])
-        doc_ref = db.document('companies/' + company_code + '/branches/' + branch_key)
-        doc_ref.set(branch)
+    # Item groups
+    for group in sql.get_stock_groups():
+        print(f'Uploading stock group {_escape_key(group.code)}')
+        doc_ref = db.document(f'itemGroups/{_escape_key(group.code)}')
+        doc_ref.set(group.to_dict())
+
+    # Customer
+    customers = {}
+    for customer in sql.get_customers():
+        customers[customer.code] = customer
+
+    for branch in sql.get_customer_branch():
+        customers[branch.customer_code].branch.append(branch)
+
+    for customer_code, customer in customers.items():
+        print(f'Uploading customer {_escape_key(customer_code)}')
+        doc_ref = db.document(f'customers/{_escape_key(customer_code)}')
+        doc_ref.set(customer.to_dict())
+
+    # Agent
+    for agent in sql.get_agents():
+        print(f'Uploading agent {_escape_key(agent.code)}')
+        doc_ref = db.document(f'agents/{_escape_key(agent.code)}')
+        doc_ref.set(agent.to_dict())
 
 
-if __name__ == '__main__':
-    start()
+with Sql() as sql:
+    sql.login()
+    upload_master_data()
