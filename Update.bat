0cb466bebbbd2685c8ab8df7bdd96df196b69c5c
springmaple <springmaple@live.com.my>
2019-10-28 22:43:33 +0800

    Fix updater

--- Update.bat
+++ Update.bat
@@ -1,8 +1,8 @@
-@ECHO OFF
 SET CURRENT_DIR=%cd%
 SET SCRIPT_DIR=%~dp0
 CD /D %SCRIPT_DIR%
-git checkout master && git pull
+git checkout master
+git pull
 FOR /F %a in ('git describe --abbrev=0 --tags') do git checkout %a
 pip install -r requirements.txt
 CD /D %CURRENT_DIR%
