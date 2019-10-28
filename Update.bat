d0e9530bdcfcb69fb5bf7f824bd590e5b80a7fab
springmaple <springmaple@live.com.my>
2019-10-28 22:36:21 +0800

    Change updater to use latest tag

--- Update.bat
+++ Update.bat
@@ -2,7 +2,8 @@
 SET CURRENT_DIR=%cd%
 SET SCRIPT_DIR=%~dp0
 CD /D %SCRIPT_DIR%
-git pull
+git checkout master && git pull
+FOR /F %a in ('git describe --tags') do git checkout %a
 pip install -r requirements.txt
 CD /D %CURRENT_DIR%
 ECHO OK...
