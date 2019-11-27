adbf5ffc8250297fb5eb81f41254e39262c8cc59
springmaple <springmaple@live.com.my>
2019-11-28 02:32:33 +0800

    Update version v1.1.1 -> v1.1.2

--- src/constants.py
+++ src/constants.py
@@ -3,7 +3,7 @@ import os
 import tempfile
 
 APP_NAME = "Let'sGO"
-APP_VERSION = 'v1.1.1'
+APP_VERSION = 'v1.1.2'
 TMP_DIR = os.path.join(os.path.abspath(tempfile.gettempdir()), 'LetsGO')
 
 os.makedirs(TMP_DIR, exist_ok=True)
